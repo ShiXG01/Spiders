3592a1f0c1a34c97b6f78f51f26dc201fa015e48
liang <1843440138@qq.com>
2023-05-04 22:06:45 +0800

    下载小说

---
+++ xiaoshuo/xiaoshuo/spiders/byzww.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class ByzwwSpider(scrapy.Spider):
+    name = "byzww"
+    allowed_domains = ["81zw.cc"]
+    start_urls = ["https://www.81zw.cc/book/24/29601599.html"]
+
+    def parse(self, response):
+        title = response.xpath('//*[@id="book"]/div[2]/h1/text()').extract_first()
+        content = ''.join(response.xpath('//*[@id="content"]/text()').extract()).replace('        ', '').replace('\r\r','\n')
+
+        yield {
+            'title': title,
+            'content': content
+        }
+
+        next_url = response.xpath('//*[@id="book"]/div[2]/div[4]/ul/li[3]/a/@href').extract_first()
+        # base_url = 'https://www.81zw.cc{}'.format(next_url)
+
+        if next_url.find('.html') != -1:
+            yield scrapy.Request(response.urljoin(next_url), callback=self.parse)
