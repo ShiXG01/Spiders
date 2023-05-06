b96ce3c5cf7f9919487546d810c21cd659dacba1
liang <1843440138@qq.com>
2023-05-06 23:24:32 +0800

    开启管道

--- lianjia/lianjia/pipelines.py
+++ lianjia/lianjia/pipelines.py
@@ -9,5 +9,21 @@ from itemadapter import ItemAdapter
 
 
 class LianjiaPipeline:
+    def open_spider(self, spider):
+        self.file = open('house.txt', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
+        info = str({
+            '标题': item['title'],
+            '房租': item['price'],
+            '组凭方法': item['leasingMethod'],
+            '房屋类型': item['houseType'],
+            '楼层': item['Floor'],
+            '朝向': item['Face'],
+            '面积': item['Area']
+        }) + '\n'
+        self.file.write(info)
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
