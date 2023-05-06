5ae6281133681c1aeb9e79ffd86ca68dc9656577
liang <1843440138@qq.com>
2023-05-06 23:24:05 +0800

    定义item

--- lianjia/lianjia/spiders/lianjia.py
+++ lianjia/lianjia/spiders/lianjia.py
@@ -10,7 +10,7 @@ class LianjiaSpider(scrapy.Spider):
     # allowed_domains = ["gz.lianjia.com"]
     start_urls = ["https://gz.lianjia.com/zufang/"]
 
-    num = 10
+    num = 1
     page = 1
 
     def parse(self, response):
@@ -25,4 +25,12 @@ class LianjiaSpider(scrapy.Spider):
 
     def parse_detail(self, response):
         items = LianjiaItem()
-        items['title'] = response.xpath()
+        items['title'] = response.xpath('/html/body/div[3]/div[1]/div[3]/p/text()').extract_first().split('·')[1].rstrip('                  ')
+        items['price'] = response.xpath('//*[@id="aside"]/div[1]/span/text()').extract_first() + '/月'
+        items['leasingMethod'] = response.xpath('//*[@id="aside"]/ul/li[1]/text()').extract_first()
+        items['houseType'] = response.xpath('//*[@id="aside"]/ul/li[2]/text()').extract_first()
+        items['Floor'] = response.xpath('//*[@id="info"]/ul[1]/li[8]/text()').extract_first().split('：')[1]
+        items['Face'] = response.xpath('//*[@id="info"]/ul[1]/li[3]/text()').extract_first().split('：')[1]
+        items['Area'] = response.xpath('//*[@id="info"]/ul[1]/li[2]/text()').extract_first().split('：')[1]
+        # items['houseNumber'] = response.xpath('/html/body/div[3]/div[1]/div[3]/div[1]/i/text()').extract_first()
+        yield items
